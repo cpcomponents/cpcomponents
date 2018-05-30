3d7fcffe8184e5b5ebd86aedd8161c877f80a9b3
casyjs <1boris231@gmail.com>
2018-05-30 14:40:37 -0700

    created AniColorLink / going to test with casyjs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import BoxyLanding from './components/LandingPages/BoxyLanding';
+import AniColorLink from './components/Links/AniColorLink';
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <h2>Copy Paste Components</h2> */}
-        <BoxyLanding />
+        <h2>Copy Paste Components</h2>
+        <AniColorLink href="http://www.casyjs.com">AniColorLink</AniColorLink>
       </div>
     );
   }
---
+++ src/components/Links/AniColorLink.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const A = styled.a`
+  text-decoration: none;
+  color: #000;
+  font-size: 20px;
+  padding-right: 15px;
+  font-weight: normal;
+  position: relative;
+  z-index: 2;
+
+  &::before {
+    content: '';
+    background: #bfdbff;
+    position: absolute;
+    top: 55%;
+    left: 0;
+    width: 100%;
+    height: 15px;
+    transform: scaleX(0);
+    transform-origin: 0;
+    transition: transform 700ms cubic-bezier(0.78, -0.03, 0, 1.05);
+    z-index: -1;
+  }
+
+  &:hover::before {
+    transform: scaleX(1);
+  }
+`;
+
+const AniColorLink = props => {
+  return <A href={props.href}>{props.children}</A>;
+};
+
+export default AniColorLink;
