1318fe807e4267ba444df3fb5d2a622aab41e95d
casyjs <1boris231@gmail.com>
2018-06-02 13:50:34 -0700

    added FlattenHamToggle

---
+++ src/components/Navigation/FlattenHamToggle.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const MainWrapper = styled.div`
+  /* ---- CSS Variables Section ----- */
+  --menu-width: ${props => (props.width ? props.width : '50px')};
+  --menu-height: ${props => (props.width ? props.height : '30px')};
+  --bar-thickness: ${props => (props.thickness ? props.thickness : '4px')};
+  --trans-speed: 150ms;
+  /* -------------------------------- */
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+`;
+
+const SubWrapper = styled.div`
+  width: var(--menu-width);
+  height: var(--menu-height);
+  position: relative;
+`;
+
+const Menu = styled.div`
+  background: black;
+  position: absolute;
+  width: 100%;
+  height: var(--bar-thickness);
+  top: 50%;
+  transform: translateY(-50%);
+
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: black;
+    transition: top var(--trans-speed) ease-in;
+  }
+
+  &::before {
+    top: ${props =>
+      props.toggleActive
+        ? '0'
+        : 'calc((var(--menu-height) / 2 - (var(--bar-thickness) / 2)) * -1)'};
+  }
+
+  &::after {
+    top: ${props =>
+      props.toggleActive
+        ? '0'
+        : 'calc(var(--menu-height) / 2 - (var(--bar-thickness) / 2))'};
+  }
+`;
+
+class FlattenHamToggle extends Component {
+  state = {
+    toggleActive: false
+  };
+
+  handleToggle = () => {
+    this.setState({
+      toggleActive: !this.state.toggleActive
+    });
+  };
+
+  render() {
+    return (
+      <MainWrapper
+        onClick={this.handleToggle}
+        width={this.props.width}
+        height={this.props.height}
+        thickness={this.props.thickness}
+      >
+        <SubWrapper>
+          <Menu
+            toggleActive={
+              this.props.toggleActive
+                ? this.props.toggleActive
+                : this.state.toggleActive
+            }
+          />
+        </SubWrapper>
+      </MainWrapper>
+    );
+  }
+}
+
+export default FlattenHamToggle;
