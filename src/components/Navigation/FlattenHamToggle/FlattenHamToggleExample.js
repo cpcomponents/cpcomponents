192db1933775389d2c81ba597c34b2a6157b5398
casyjs <1boris231@gmail.com>
2018-06-06 10:20:23 -0700

    testing ussage document for display on casyjs

---
+++ src/components/Navigation/FlattenHamToggle/FlattenHamToggleExample.js
@@ -0,0 +1,12 @@
+// Example ussage of component toggled with interal state
+//
+<FlattenHamToggle width="40px" height="23px" thickness="3px" />;
+
+// Example ussage of component toggled with external state passed via prop
+//
+<FlattenHamToggle
+  toggleActive={this.state.toggle}
+  width="40px"
+  height="23px"
+  thickness="3px"
+/>;
