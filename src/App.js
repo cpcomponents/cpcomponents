8acc0dfdcff0a225234dbe1cbe64b85d069c1e0b
casyjs <1boris231@gmail.com>
2018-06-08 14:51:39 -0700

    bs commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,27 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 import ArbitraryComponent from './components/Templates/ArbitraryComponent';
-import ArbitraryClassComponent from './components/Templates/ArbitraryClassComponent';
+// import ArbitraryClassComponent from './components/Templates/ArbitraryClassComponent';
 
 const HeaderWrapper = styled.div`
   background: #3f9081;
   padding: 0 20px;
 `;
 
-const Name = styled.h1`
-  letter-spacing: 1px;
-  color: white;
-  padding: 30px 0 20px;
-  font-weight: bold;
-  text-align: center;
-`;
+// const Name = styled.h1`
+//   letter-spacing: 1px;
+//   color: white;
+//   padding: 30px 0 20px;
+//   font-weight: bold;
+//   text-align: center;
+// `;
 
-const WorkspaceDesc = styled.p`
-  color: white;
-  padding-bottom: 30px;
-  font-size: 20px;
-  text-align: center;
-`;
+// const WorkspaceDesc = styled.p`
+//   color: white;
+//   padding-bottom: 30px;
+//   font-size: 20px;
+//   text-align: center;
+// `;
 
 class App extends Component {
   render() {
--- src/components/Navigation/HamToXToggle/HamToXToggleExample.js
+++ src/components/Navigation/HamToXToggle/HamToXToggleExample.js
@@ -1,4 +1,4 @@
-// Example ussage of component toggled with interal state //
+// Example ussage of component toggled with internal state //
 <HamToXToggle width="40px" height="23px" thickness="3px" />;
 
 // Example ussage of component toggled with external state passed via prop //
