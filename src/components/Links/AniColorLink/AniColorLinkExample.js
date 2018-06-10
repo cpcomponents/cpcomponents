86c9e7f7a78c789aab11c49d0cf7131a8d3995bd
casyjs <1boris231@gmail.com>
2018-06-09 20:59:06 -0700

    added http:// back to aniColor Example now that ExpandingUsageBox uses regex

--- src/components/Links/AniColorLink/AniColorLinkExample.js
+++ src/components/Links/AniColorLink/AniColorLinkExample.js
@@ -1,6 +1,6 @@
 /* Example usage of component with optional props - href prop is manditory */
 <AniColorLink
-  href="www.casyjs.com"
+  href="http://www.casyjs.com"
   aniColor="var(--pri-color)"
   fontColor="var(--black)"
 >
