5b4bbacb788ce053369c601b0d208b6c4382d480
casyjs <1boris231@gmail.com>
2018-07-01 19:34:58 -0700

    added SpinningBorder

---
+++ src/components/Loaders/SpinningBorder/SpinningBorderExample.js
@@ -0,0 +1,9 @@
+/* Example usage of component with optional props */
+<SpinningBorder
+  priColor="#26a69a"
+  secColor="#005041"
+  innerColor="#fff"
+  speed="876ms"
+  size="85px"
+  thickness="20px"
+/>;
